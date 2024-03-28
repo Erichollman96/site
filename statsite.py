c1fdbbf8c262aa5571750960238b68539a62bbe5
Erichollman96 <erichollman96@gmail.com>
2024-03-28 01:58:27 -0400

    Update statsite.py
    cleanup

--- statsite.py
+++ statsite.py
@@ -14,54 +14,54 @@ url = 'https://www.pro-football-reference.com/years/2022/rushing.htm'
 
 df = pd.read_html(url, header=1)[0]
 
-
-#####################  P L O T T I N G ###########################
-
-
 # sets limit of rows scraped
 df = df[:10]
 
 #casting everything besides "Player" to type float
 df['Yds'] = df['Yds'].astype('float')
 
+#####################  P L O T T I N G ###########################
+
+
+
 # data values assigned to axes
-y = df['Yds']
-x = df['Player']
+#y = df['Yds']
+#x = df['Player']
 
-@app.route('/generate_graph', methods=['POST'])
-def generate_graph():
-	# data values assigned to axes
-	y = df['Yds']
-	x = df['Player']
-
+#@app.route('/generate_graph', methods=['POST'])
+#def generate_graph():
+#	# data values assigned to axes
+#	y = df['Yds']
+#	x = df['Player']
+#
 # Create a bar graph
-plt.bar(x, y)
-
-# Add labels and a title
-plt.ylabel('Yds')
-plt.xlabel('Player')
-
-
-
-#rotates x-axis labels
-plt.xticks(rotation=90)
-
-#title
-plt.title('StatShow')
-
-# Show the plot
-plt.show()
-
-# Clear the plot
-plt.clf()
-
-# Return the graph as a downloadable file
-# return send_file(buffer, as_attachment=True, download_name='graph.png', mimetype='image/png')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-############################################################
+#plt.bar(x, y)
+#
+## Add labels and a title
+#plt.ylabel('Yds')
+#plt.xlabel('Player')
+#
+#
+#
+##rotates x-axis labels
+#plt.xticks(rotation=90)
+#
+##title
+#plt.title('StatShow')
+#
+## Show the plot
+#plt.show()
+#
+## Clear the plot
+#plt.clf()
+#
+## Return the graph as a downloadable file
+## return send_file(buffer, as_attachment=True, download_name='graph.png', mimetype='image/png')
+#
+#if __name__ == '__main__':
+    #app.run(debug=True)
+#
+#############################################################
 
 pd.set_option('display.max_rows', 50)
 pd.set_option('display.max_columns', 500)
